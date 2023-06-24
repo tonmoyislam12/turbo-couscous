1bec89603143b5948942c54df760e3ece169e9fb
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-06-25 01:30:36 +0600

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,4 +1,4 @@
 gdown 1Uc5wEqzi2gxtGJRyrRKp5gxBoSsBqEzx
 mediainfo *.mkv
-ffmpeg -i *.mkv -c:v libx265 -crf 28 -preset superfast output.mkv
+ffmpeg -i *.mkv -c:v libx265 -crf 28 -preset ultrafast output.mkv
 mediainfo output.mkv
